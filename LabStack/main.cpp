1ee44f891c0a3d31741eacefbe7e8654cd17cfa7
alex <lexathebest23@gmail.com>
2017-10-28 16:04:29 +0300

    modificated main.cpp

--- LabStack/main.cpp
+++ LabStack/main.cpp
@@ -4,5 +4,27 @@
 using namespace std;
 
 void main() {
-	cout << "Hello" << endl;
+	TStack<int> s;
+	int exit = 1, num = 0;
+
+	while (exit) {
+		cin >> exit;
+		switch (exit)
+		{
+		case 1:
+			cin >> num;
+			s.push(num);
+			break;
+		case 2: 
+			if (s.isEmpty()) break;
+			cout << s.pop() << endl;
+			break;
+		case 3:
+			if (s.isEmpty()) break;
+			cout << s.top() << endl;
+			break;
+		default:
+			break;
+		}
+	}
 }
